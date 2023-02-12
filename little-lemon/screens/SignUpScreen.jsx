8016ea3566bde92b1e235d62a563896ec5d86060
Amol derick soans <amol.optimus@gmail.com>
2023-02-12 22:12:42 +0530

    reset the app

---
+++ little-lemon/screens/SignUpScreen.jsx
@@ -0,0 +1,12 @@
+import React  from 'react';
+import { SafeAreaView } from 'react-native';
+const SignUpScreen = () =>{
+
+return(
+        <SafeAreaView>
+ 
+        </SafeAreaView>
+    );
+}
+
+export default SignUpScreen
